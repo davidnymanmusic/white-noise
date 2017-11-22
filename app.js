9cd52371a50a2ee069a6bd30b3901d19acec2826
David Nyman <davenyman@gmail.com>
2017-11-22 11:39:33 -0500

    added site

---
+++ app.js
@@ -0,0 +1,5 @@
+var h1 = document.getElementById("q1head");
+
+h1.addEventListener("click", function(){
+  h1.style.fontSize = "300px";
+});
---
+++ assets/noise.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#container").hide(0).delay(500).fadeIn(3000)
+    $("body").hide(0).delay(500).fadeIn(3000)
+
+
+});
