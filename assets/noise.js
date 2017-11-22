53bd4b99c73512dffb7ae4ca3cbe5827dc72be14
David Nyman <davenyman@gmail.com>
2017-11-22 13:02:19 -0500

    added settings

--- assets/noise.js
+++ assets/noise.js
@@ -2,5 +2,11 @@ $(document).ready(function(){
     $("#container").hide(0).delay(500).fadeIn(3000)
     $("body").hide(0).delay(500).fadeIn(3000)
 
+    $("#buttonSettings").click(function(){
+           $("#set").show("slow");
+       });
+    $("#buttonSettings").dblclick(function(){
+           $("#set").hide("slow");
+       });
 
 });
