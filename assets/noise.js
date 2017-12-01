d2adaa81ac2fb0c284521defacf97c26561636d0
David Nyman <davenyman@gmail.com>
2017-12-01 13:01:42 -0500

    added tone

--- assets/noise.js
+++ assets/noise.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){
-    $("#container").hide(0).delay(500).fadeIn(3000)
-    $("body").hide(0).delay(500).fadeIn(3000)
+$(document).ready(function() {
+  $("#container").hide(0).delay(500).fadeIn(3000)
+  $("body").hide(0).delay(500).fadeIn(3000)
 
-    $("#buttonSettings").click(function(){
-           $("#set").show("slow");
-       });
-    $("#buttonSettings").dblclick(function(){
-           $("#set").hide("slow");
-       });
+  $("#buttonSettings").click(function() {
+    $("#set").show("slow");
+  });
+  $("#buttonSettings").dblclick(function() {
+    $("#set").hide("slow");
+  });
 
 });
 
-var pinkNoise = new Tone.Noise("pink").toMaster();
+
 var vol01 = document.querySelector('#volume1')
 
 vol01.addEventListener('input', function() {
@@ -19,11 +19,31 @@ vol01.addEventListener('input', function() {
   pinkNoise.volume.value = vol;
 })
 
-
-document.querySelector('.playToggle').addEventListener('change', function(e){
-	if (e.target.checked){
-  	pinkNoise.start()
+var pinkNoise = new Tone.Noise("pink").toMaster();
+document.querySelector('#playToggle1').addEventListener('change', function(e) {
+  if (e.target.checked) {
+    pinkNoise.start()
   } else {
-  	pinkNoise.stop()
+    pinkNoise.stop()
   }
 });
+
+var sound01 = new Tone.Oscillator({
+  "partials": [6, 5, 4, 3, 2, 1],
+  "type": "custom",
+  "frequency": 426.7,
+  "volume": -25
+}).toMaster()
+
+document.querySelector('#playToggle2').addEventListener('change', function(e) {
+  if (e.target.checked) {
+    sound01.start()
+  } else {
+    sound01.stop()
+  }
+});
+var vol02 = document.querySelector('#volume2')
+vol02.addEventListener('input', function() {
+  var vol = document.querySelector('#volume2').value;
+  sound01.volume.value = vol;
+})
