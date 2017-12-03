964b1c214277399302458ab9c5e8b25374b4ab37
David Nyman <davenyman@gmail.com>
2017-12-03 17:17:55 -0500

    added ambient sound

--- assets/noise.js
+++ assets/noise.js
@@ -90,7 +90,6 @@ document.querySelector('#playTogglePink').addEventListener('change', function(e)
 
 
 
-
 var sound01 = new Tone.Oscillator({
   "partials": [11, 1],
   "type": "custom",
@@ -105,13 +104,93 @@ var sound02 = new Tone.Oscillator({
   "volume": -30
 }).toMaster();
 
+var rootChakra = new Tone.Oscillator({
+  "type": "sine",
+  "frequency": 194.18,
+  "volume": -30
+}).toMaster();
+var rootChakra2 = new Tone.Oscillator({
+  "type": "sine",
+  "frequency": 396,
+  "volume": -30
+}).toMaster();
+
+var thirdEye = new Tone.Oscillator({
+  "type": "sine",
+  "frequency": 852,
+  "volume": -30
+}).toMaster();
+var thirdEye2 = new Tone.Oscillator({
+  "type": "sine",
+  "frequency": 83,
+  "volume": -30
+}).toMaster();
+
 
 document.querySelector('#playToggle2').addEventListener('change', function(e) {
   if (e.target.checked) {
-    sound01.start();
+    rootChakra.start();
+    rootChakra2.start();
+
+    // sound01.start();
     // sound02.start();
   } else {
-    sound01.stop();
+    rootChakra.stop();
+    rootChakra2.stop();
+    // sound01.stop();
+    // sound02.stop();
+  }
+});
+
+var sound = new Howl({
+  src: ['https://k003.kiwi6.com/hotlink/rhsa9gtye9/ocean_2.mp3'],
+  volume: 0.8,
+});
+
+
+document.querySelector('#playToggle3').addEventListener('change', function(e) {
+  if (e.target.checked) {
+sound.play();
+  } else {
+sound.stop();
+  }
+});
+var vol03 = document.querySelector('#volume3')
+vol03.addEventListener('input', function() {
+  var vol = document.querySelector('#volume3').value;
+   Howler.volume(vol);
+  console.log(vol);
+
+});
+
+
+document.querySelector('#chakraToggle').addEventListener('change', function(e) {
+  if (e.target.checked) {
+    rootChakra.start();
+    rootChakra2.start();
+
+    // sound01.start();
+    // sound02.start();
+  } else {
+    rootChakra.stop();
+    rootChakra2.stop();
+    // sound01.stop();
+    // sound02.stop();
+  }
+});
+document.querySelector('#chakraToggle1').addEventListener('change', function(e) {
+  if (e.target.checked) {
+    thirdEye.start();
+    thirdEye2.start();
+    rootChakra.stop();
+    rootChakra2.stop();
+
+    // sound01.start();
+    // sound02.start();
+  } else {
+    thirdEye.stop();
+    thirdEye2.stop();
+    // sound01.stop();
     // sound02.stop();
   }
 });
@@ -119,6 +198,10 @@ var vol02 = document.querySelector('#volume2')
 vol02.addEventListener('input', function() {
   var vol = document.querySelector('#volume2').value;
   sound01.volume.value = vol;
+  rootChakra.volume.value = vol;
+  rootChakra2.volume.value = vol;
+  thirdEye.volume.value = vol;
+  thirdEye2.volume.value = vol;
   // sound02.volume.value = vol;
 });
 
